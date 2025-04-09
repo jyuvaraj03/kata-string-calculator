fbb70546fbd07071b83298a410269126b56b3145
Yuvaraj J <jyuvaraj03@gmail.com>
2025-04-09 15:50:29 +0000

    Refactor: Move parsing logic to separate method

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,17 @@
 class StringCalculator
   def add(numbers_str)
-    numbers = numbers_str.split(',')
+    numbers = parse_numbers(numbers_str)
 
     sum = 0
     numbers.each do |number|
       sum += number.to_i
     end
     sum
+
+  private
+
+  def parse_numbers(numbers_str)
+    split_number_strs = numbers_str.split(',')
+    split_number_strs.map(&:to_i)
   end
 end
