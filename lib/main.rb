20294d60754edbd37cf9a06f3f0bea7461e11c04
Yuvaraj J <jyuvaraj03@gmail.com>
2025-04-09 15:52:10 +0000

    Refactor: Use inject method to sum numbers

--- lib/main.rb
+++ lib/main.rb
@@ -2,11 +2,8 @@ class StringCalculator
   def add(numbers_str)
     numbers = parse_numbers(numbers_str)
 
-    sum = 0
-    numbers.each do |number|
-      sum += number.to_i
-    end
-    sum
+    numbers.inject(0, :+)
+  end
 
   private
 
